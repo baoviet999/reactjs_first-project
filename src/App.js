a5112f2b01507ecfe8c9d7cbcb2228523d6ff0fe
BaoViet <baobaovietviet123@gmail.com>
2022-01-01 11:28:21 +0700

    TEST 3

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Bao Viet 123456
+          Nguyen bAO vIERT a
         </p>
         <a
           className="App-link"
